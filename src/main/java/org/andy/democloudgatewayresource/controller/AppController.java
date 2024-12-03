ef1cadfa0c4e2208653ec45d9deda934aaed705f
Dung1109 <124704925+Dung1109@users.noreply.github.com>
2024-12-04 02:27:40 +0700

    add authority

--- src/main/java/org/andy/democloudgatewayresource/controller/AppController.java
+++ src/main/java/org/andy/democloudgatewayresource/controller/AppController.java
@@ -1,13 +1,22 @@
 package org.andy.democloudgatewayresource.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@EnableMethodSecurity
 public class AppController {
 
     @GetMapping("/secret")
     public String secret() {
         return "secret for you";
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasAuthority('SCOPE_u.test')")
+    public String admin() {
+        return "admin for you";
     }
 }
